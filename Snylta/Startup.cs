886522cf252a51ae12855cf09d11e17513afc3f5
Magnus Tidqvist <magnus.tidqvist@outlook.com>
2019-02-18 17:08:05 +0100

    Lazy loading som fungerar på things/snylta

--- Snylta/Startup.cs
+++ Snylta/Startup.cs
@@ -50,7 +50,10 @@ namespace Snylta
                 .AddDefaultUI()
                 .AddDefaultTokenProviders();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddJsonOptions(
+                    options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
