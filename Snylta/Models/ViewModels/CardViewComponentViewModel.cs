7c6dee5e6dc9db75dd444999613ba15825614fe3
Magnus Tidqvist <magnus.tidqvist@outlook.com>
2019-02-20 13:40:02 +0100

    Gjort en component av Card som kan återanvändas för att visa upp produkter

---
+++ Snylta/Models/ViewModels/CardViewComponentViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snylta.Models.ViewModels
+{
+    public class CardViewComponentViewModel
+    {
+        public Thing Thing { get; set; }
+        public int CardId { get; set; }
+    }
+}
